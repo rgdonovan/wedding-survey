9312c8cba3867b381b0b3f022ef67c62cea1c410
Regina Donovan <reginadonovan191@gmail.com>
2022-04-08 19:37:36 -0400

    update rsvp info

--- src/App.js
+++ src/App.js
@@ -15,12 +15,14 @@ function App() {
         <h2 className="info__datetime">July 03 2022 <span>at</span> 6 PM</h2>
         <h3 className="info__location">4000 Estancia Boulevard, Wesley Chapel, Florida</h3>
         <p className="info__text">We'd love to celebrate with you. Please RSVP below!</p>
+        <h2 className="info__deadline">Please RSVP by<br /> <span>April 30th</span></h2>
+        <p className="info__text">(its a short turnaround, we know)</p>
         <nav>
           <a href="https://forms.gle/DzH5CZ1R2ZX4mm2S9">RSVP</a>
           {showGifts ? (
             <div className="gift-info">
-              <p>To be honest, we don't have a ton of material needs at the moment. If you'd like to give a gift, please click <a href="">here</a></p>
-              <p>We'd also happily accept gifts for our honeymoon! <a href="https://venmo.com/code?user_id=2243801024299008025&created=1649459328.441108&printed=1">here</a></p>
+              <p>Our tiny apartment doesn't leave much room for gifts, but we'd appreciate anything on this list: <a href="">here</a></p>
+              <p>We'd also happily accept &#128184; for our honeymoon! <a href="https://venmo.com/code?user_id=2243801024299008025&created=1649459328.441108&printed=1">here</a></p>
             </div>
           ) : (
             <button onClick={() => setShowGifts(!showGifts)}>Gift Registry</button>
