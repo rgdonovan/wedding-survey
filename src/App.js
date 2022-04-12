e8bf94132b86a3fcbadc27b30bf8828c5f983c3b
Regina Donovan <reginadonovan191@gmail.com>
2022-04-11 21:25:17 -0400

    changed layout, removed giftser link

--- src/App.js
+++ src/App.js
@@ -12,17 +12,20 @@ function App() {
         </div>
       </div >
       <div className="info">
-        <h2 className="info__datetime">July 03 2022 <span>at</span> 6 PM</h2>
-        <h3 className="info__location">4000 Estancia Boulevard, Wesley Chapel, Florida</h3>
+        <p className="info__top">(its a short turnaround, we know)</p>
+        <h2 className="info__date">07 / 03 / 22</h2>
+        <h3 className="info__location">4000 Estancia Boulevard, Wesley Chapel, Fl</h3>
+        <div className="timebox">
+          <h2 className="timebox__time"><span>Ceremony </span><br />5:30 PM</h2>
+          <h2 className="timebox__time"><span> Reception </span><br />6:00 PM</h2>
+        </div>
         <p className="info__text">We'd love to celebrate with you. Please RSVP below!</p>
         <h2 className="info__deadline">Please RSVP by<br /> <span>April 30th</span></h2>
-        <p className="info__text">(its a short turnaround, we know)</p>
         <nav>
           <a href="https://forms.gle/DzH5CZ1R2ZX4mm2S9">RSVP</a>
           {showGifts ? (
             <div className="gift-info">
-              <p>Our tiny apartment doesn't leave much room for gifts, but we'd appreciate anything on this list: <a href="">here</a></p>
-              <p>We'd also happily accept &#128184; for our honeymoon! <a href="https://venmo.com/code?user_id=2243801024299008025&created=1649459328.441108&printed=1">here</a></p>
+              <p>Our tiny apartment doesn't leave much room for gifts, but we'd appreciate &#128184; for our future home! <a href="https://venmo.com/code?user_id=2243801024299008025&created=1649459328.441108&printed=1">here</a></p>
             </div>
           ) : (
             <button onClick={() => setShowGifts(!showGifts)}>Gift Registry</button>
