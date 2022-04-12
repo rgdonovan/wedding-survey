c8d3616a87dbdbb1676e2874d1666ad684f34ccb
Regina Donovan <reginadonovan191@gmail.com>
2022-04-12 19:11:46 -0400

    updated gift registry again

--- src/App.js
+++ src/App.js
@@ -24,8 +24,10 @@ function App() {
           <a target="_blank" rel="noreferrer" href="https://forms.gle/K7k3D13DiYFzpGiT6">RSVP</a>
           {showGifts ? (
             <div className="gift-info">
-              <p>Our tiny apartment doesn't leave much room for gifts, but we'd appreciate anything on this list: <a target="_blank" href="https://www.giftster.com/gift/public/8bs8s/">here</a></p>
-              <p>We'd also happily accept &#128184; as we save towards a new home! <a target="_blank" rel="noreferrer" href="https://venmo.com/code?user_id=2243801024299008025&created=1649459328.441108&printed=1">here</a></p>
+              <p>Our tiny apartment doesn't leave much room for gifts, but if you'd like to get us a gift we'd appreciate anything on this list: <a target="_blank" href="https://www.giftster.com/gift/public/8bs8s/"> &#127873; gift list</a></p>
+              <p>Mostsly, we'd like to dedicate our registry towards a home for our future family. If you'd like, you can contribute below. And if not, your presence on our big day is enough &#10084;
+                <a target="_blank" rel="noreferrer" href="https://venmo.com/code?user_id=2243801024299008025&created=1649459328.441108&printed=1">here</a>
+              </p>
             </div>
           ) : (
             <button onClick={() => setShowGifts(!showGifts)}>Gift Registry</button>
